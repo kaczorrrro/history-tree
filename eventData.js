6495273c1d3abdf8d798102699a59ec452c16098
Karolina Wierbol <k.a.wierbol@gmail.com>
2018-02-15 20:16:49 +0100

    class

--- eventData.js
+++ eventData.js
@@ -1,5 +1,6 @@
 class EventData{
-  constructor(eventId, type, time, origin = null, name, url){
+  constructor(tabId, eventId, type, time, origin = null, name, url){
+    this.tabId = tabId;
     this.eventId = eventId;
     this.type = type;
     this.time = time;
